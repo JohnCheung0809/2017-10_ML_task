778506ecdf74fd81ed32bc7560c7397681c921e7
JohnCheung <JohnCheung0809@gmail.com>
2017-10-16 14:26:23 +0800

    JohnCheung's task in Oct.

---
+++ id_code.py
@@ -0,0 +1,33 @@
+# -- coding:utf-8 --
+
+from PIL import Image
+import os
+import pytesseract
+from pyocr import tesseract
+
+#create a list stored the addresses of examples
+gifList=[]
+
+def eachFile(filepath):
+    pathDir =  os.listdir(filepath)
+    for allDir in pathDir:
+        child = os.path.join('%s/%s' % (filepath, allDir))
+        if "gif" in child:
+            gifList.append(child)
+eachFile("/Users/zhangmingxuan/Desktop/python_captcha/examples") #this is the folder to store some examples
+
+#this is the way of shiyanlou to modify the gifs
+for i in gifList:
+    im = Image.open(i)
+    im.convert("P")
+    im2 = Image.new("P", im.size, 255)
+    for x in range(im.size[1]):
+        for y in range(im.size[0]):
+            pix = im.getpixel((y, x))
+            if pix == 220 or pix == 227:  # these are the numbers to get
+                im2.putpixel((y, x), 0)
+#save the modified gifs as convert.gif
+    im2.save('convert.gif')
+#use tesseract
+    code = pytesseract.image_to_string(im2)
+    print '%s : %s' % (i, code)
