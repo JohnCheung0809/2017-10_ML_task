8a28cb064680b649f105a370b07913a6795ffce1
JohnCheung <JohnCheung0809@gmail.com>
2017-10-28 16:25:17 +0800

    renew

--- id_code.py
+++ id_code.py
@@ -16,6 +16,7 @@ def eachFile(filepath):
             gifList.append(child)
 eachFile("/Users/zhangmingxuan/Desktop/python_captcha/examples") #this is the folder to store some examples
 
+
 #this is the way of shiyanlou to modify the gifs
 for i in gifList:
     im = Image.open(i)
@@ -27,7 +28,8 @@ for i in gifList:
             if pix == 220 or pix == 227:  # these are the numbers to get
                 im2.putpixel((y, x), 0)
 #save the modified gifs as convert.gif
-    im2.save('convert.gif')
+
+    im2.save('convertpic/'+i[-10:])
 #use tesseract
     code = pytesseract.image_to_string(im2)
-    print '%s : %s' % (i, code)
+    print '%s : %s' % (i[-10:], code)
